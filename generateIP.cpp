dd506e4eb6f003b0ee6cc3be838b200ad4e7dfe7
dande7lion <halbiniak.natalia@gmail.com>
2020-05-07 21:12:45 +0200

    Optimization of the generateIP function

--- generateIP.cpp
+++ generateIP.cpp
@@ -5,31 +5,37 @@
 
 std::vector<std::string> generateIP(const std::string& s){
     std::vector<std::string> result;
-    std::string tab[4];
+    std::string prepareIP[4];
     int length = s.length();
     if(length < 4 || length > 12){
         std::cout << "Incorrect data. Please try again" << std::endl;
         return result;
     }
-    for(int i = 0; i < length-1; i++){
-        for(int j = i + 1; j < length-1; j++){
-            for(int k = j + 1; k < length-1; k++){
-                std::string tmp;
-                tab[0] = s.substr(0,i+1);
-                tab[1] = s.substr(i+1,(j+1)-(i+1));
-                tab[2] = s.substr(j+1, (k+1)-(j+1));
-                tab[3] = s.substr(k+1, length-(k+1));
-                bool suitable = true;
-                for(int l = 0; l < 4; l++){
-                    if(atoi(tab[l].c_str()) > 255){
-                        suitable = false;
-                        break;
-                    }
-                }
-                if(suitable){
-                    tmp = tab[0] + "." + tab[1] + "." + tab[2] + "." + tab[3];
-                    result.push_back(tmp);
-                }
+    for(int i = 0; i < 3; i++){
+        prepareIP[0] = s.substr(0,i+1);
+        if(atoi(prepareIP[0].c_str()) > 255)
+            break;
+        for(int j = i + 1; j < i + 4; j++){
+            if(j >= length-2)
+                break;
+            prepareIP[1] = s.substr(i+1,(j+1)-(i+1));
+                if(atoi(prepareIP[1].c_str()) > 255)
+                    break;
+            for(int k = j + 1; k < j + 4; k++){
+                if(k >= length-1)
+                    break;
+                prepareIP[2] = s.substr(j+1, (k+1)-(j+1));
+                prepareIP[3] = s.substr(k+1, length-(k+1));
+
+                if(atoi(prepareIP[2].c_str()) > 255)
+                    break;
+
+                if(atoi(prepareIP[3].c_str()) > 255)
+                    continue;
+
+                std::string oneCorrectAddress;
+                    oneCorrectAddress = prepareIP[0] + "." + prepareIP[1] + "." + prepareIP[2] + "." + prepareIP[3];
+                    result.push_back(oneCorrectAddress);
             }
         }
     }
